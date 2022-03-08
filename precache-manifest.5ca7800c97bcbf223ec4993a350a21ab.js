82229b9dada7f5927fd83f92b08decfa89d3e38e
nfahmed11 <nfahmed11@gmail.com>
2022-03-07 21:25:40 -0600

    Updates

---
+++ precache-manifest.5ca7800c97bcbf223ec4993a350a21ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3e33d43344e9ada0054",
+    "url": "/lights-out/static/css/main.13e2323c.chunk.css"
+  },
+  {
+    "revision": "e3e33d43344e9ada0054",
+    "url": "/lights-out/static/js/main.e3e33d43.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/lights-out/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "620404c6da87e2703570027e721cdb6a",
+    "url": "/lights-out/index.html"
+  }
+];
